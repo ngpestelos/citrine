1b16ea8a0547c1a2ed647cafc96d6ebfa3712c92
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-04-01 19:08:58 +0800

    blurb

--- citrine/feeds.py
+++ citrine/feeds.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 Nestor G Pestelos Jr
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
 from couchdb import Server
 import parser
 from datetime import datetime
